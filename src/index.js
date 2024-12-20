4a73f6503d5e34ed70287f61043832546cd86549
Silvia Prieto <info@silviaprieto.com>
2024-12-20 10:26:21 +0100

    add clients get endpoints

--- src/index.js
+++ src/index.js
@@ -25,3 +25,27 @@ const port = 5001;
 server.listen(port, () => {
   console.log(`Server is running. Go to http://localhost:${port}`);
 });
+
+server.get("/clients", async (req, res) => {
+  const connection = await getDBConnection();
+  const query = "SELECT id, name, lastname, email FROM clients";
+  const [result] = await connection.query(query);
+  connection.end();
+  res.status(200).json({
+    info: { count: result.length },
+    result: result,
+  });
+});
+
+server.get("/clients/:id", async (req, res) => {
+  const id = req.params.id;
+  const connection = await getDBConnection();
+  const query = "SELECT id, name, lastname, email FROM clients WHERE id = ?";
+  const [result] = await connection.query(query, [id]);
+  connection.end();
+  if (result.length !== 1) {
+    res.status(404).json({});
+  } else {
+    res.status(200).json(result[0]);
+  }
+});
